fda638ce48c9ea15b8e057922c772825a1b6f975
gitmoneyman-es <git.es@idfinance.com>
2024-07-28 23:49:37 +0200

    ref

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+# birthday/views.py
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
@@ -7,29 +9,35 @@ from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 10
+
+
 class BirthdayMixin:
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
 
-class BirthdayFormMixin:
+class BirthdayCreateView(BirthdayMixin, CreateView):
     form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
 
 
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
-    pass
-
-
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
-    pass
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    form_class = BirthdayForm
 
 
 class BirthdayDeleteView(BirthdayMixin, DeleteView):
     pass
 
 
-class BirthdayListView(ListView):
+class BirthdayDetailView(DetailView):
     model = Birthday
-    ordering = 'id'
-    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
