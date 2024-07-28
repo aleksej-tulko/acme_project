c61682f72ff788da7f791b92c7f7e225e6725bbd
gitmoneyman-es <git.es@idfinance.com>
2024-07-28 22:24:53 +0200

    delete

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import BirthdayForm
 from .models import Birthday
@@ -25,3 +25,14 @@ def birthday_list(request):
     birthdays = Birthday.objects.all()
     context = {'birthdays': birthdays}
     return render(request, 'birthday/birthday_list.html', context)
+
+
+def delete_birthday(request, pk):
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('birthday:list')
+    return render(request, 'birthday/birthday.html', context)
+
